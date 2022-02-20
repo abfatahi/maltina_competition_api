d12989b4de86ad6b232e45c656e8d6d42dcedae6
abfatahi <abfatahi.iaf@gmail.com>
2022-02-20 16:03:53 +0100

    Feat: implemented login admin route

--- controllers/Admin/index.js
+++ controllers/Admin/index.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { AdminModel } from '../../database/models/index.js';
 
 export default () => {
@@ -37,7 +38,50 @@ export default () => {
     }
   };
 
+  const login = async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+
+      // Authentication Logic
+      const { email, password } = req.body;
+
+      const Admin = await AdminModel.findOne({ email });
+      if (!Admin)
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Failed! Invalid login details' }] });
+      // compare password
+      const isValidPassword = await bcrypt.compare(password, Admin.password);
+      if (!isValidPassword)
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Failed! Incorrect password' }] });
+
+      // Generate customer token
+      const token = jwt.sign({ email: Admin.email }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+      });
+      
+      const { name, role } = Admin;
+
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          name,
+          email,
+          role,
+        },
+        token,
+      });
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+
   return {
     register,
+    login,
   };
 };
--- routes/Admin/index.js
+++ routes/Admin/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body } from 'express-validator';
 import Controller from '../../controllers/Admin/index.js';
 
 const AdminController = Controller();
@@ -6,5 +7,24 @@ const AdminController = Controller();
 const router = express.Router();
 
 router.post('/register', AdminController.register);
+
+router.post(
+  '/login',
+  [
+    body('email', 'Failed! Email cant be blank')
+      .exists()
+      .bail()
+      .isEmail()
+      .withMessage('Invalid Email format'),
+    body('password', 'Failed! Password cant be blank')
+      .exists()
+      .bail()
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Password cant be empty'),
+  ],
+  AdminController.login
+);
 
 export default router;
